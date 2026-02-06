e69d63e2cb4eff5cbd47d008c997d89723b3ed4a
zvzvzv10 <126672233+zvzvzv10@users.noreply.github.com>
2026-02-06 13:05:23 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.dimlen.ru',
+          },
+        ],
+        destination: 'https://dimlen.ru/:path*',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     // Это разрешает оптимизацию для локальных картинок
     remotePatterns: [
